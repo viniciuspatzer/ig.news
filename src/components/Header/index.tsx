9905a06f8e5f11c8dfd3c5772094e089db62cef1
Vinicius Patzer <viniciuspatzer2@gmail.com>
2021-09-20 20:48:03 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { SignInButton } from '../SignInButton';
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" width="100%" height="100%"/>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,58 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+
+import styles from '../styles/home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for ${product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width="336px" height="521px"/>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Jbu2iKkLa8XvrkIP24l94hW');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
+}
