ea060dd38b466e97c31879ec445a3e6e3d8c9896
Vinicius Patzer <viniciuspatzer2@gmail.com>
2021-09-23 15:05:32 -0300

    new commit

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,14 +23,16 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  "checkout.session.completed"
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const buf = await buffer(req);
     const secret = req.headers['stripe-signature'];
-
+ 
     let event: Stripe.Event;
 
     try {
@@ -44,16 +46,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
-          case 'checkout.session.completed':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
 
-          const checkoutSession = event.data.object as Stripe.Checkout.Session
-             
-          await saveSubscription(
-            checkoutSession.subscription.toString(),
-            checkoutSession.customer.toString()
-          );
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
 
             break;
+
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+             
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+
+            break;
+
           default:
             throw new Error('Unhandled event.')
         }
