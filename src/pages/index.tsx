cde08d78461a8d1206c724c59b281fc4f9d0fb4b
Vinicius Patzer <viniciuspatzer2@gmail.com>
2021-09-24 18:12:15 -0300

    new commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
 
-import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
 
 interface HomeProps {
   product: {
